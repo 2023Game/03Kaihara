e6b374346bcd2ddbf2a19fe5ebc716849a13315d
kaihara-yuugo-Fukuyama <gc23009@anabuki-net.ne.jp>
2023-10-03 11:12:58 +0900

    課題７

--- 3DLv1_vs2019_00/GameProgramming/src/CModel.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CModel.h
@@ -1,6 +1,9 @@
 #pragma once
 #ifndef CMODEL_H
 #define CMODEL_H
+//vectorのインクルード
+#include <vector>
+#include "CTriangle.h"
 /*
 モデルクラス
 モデルデータの入力や表示
@@ -10,6 +13,11 @@ public:
 	//モデルファイルの入力
 	//Load(モデルファイル名, マテリアルファイル名)
 	void Load(char* obj, char* mtl);
+	//描画
+	void Render();
+private:
+	//三角形の可変長配列
+	std::vector<CTriangle> mTriangles;
 };
 
 #endif
