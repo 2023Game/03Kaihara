bea08923ec811ad498bc0e6f610ac5b0f8e7e13c
kaihara-yuugo-Fukuyama <gc23009@anabuki-net.ne.jp>
2023-10-05 12:18:19 +0900

    課題9

--- 3DLv1_vs2019_00/GameProgramming/src/CModel.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CModel.cpp
@@ -46,6 +46,8 @@ void CModel::Load(char* obj, char* mtl) {
 		printf("%s file open error￥n", mtl);
 		return;
 	}
+	//マテリアルインデックス
+	int idx = 0;
 
 	//ファイルから1行入力
 	//fgets(入力エリア,エリアサイズ,ファイルポインタ)
@@ -54,36 +56,28 @@ void CModel::Load(char* obj, char* mtl) {
 		//データを分割する
 		char str[4][64] = { "", "", "", "" };
 		//文字列からデータを4つ変数へ代入する
-		//sscanf(文字列, 変換指定子, 変数)
 		sscanf(buf, "%s %s %s %s", str[0], str[1], str[2], str[3]);
-		//文字列の比較
-		//strcmp(文字列1, 文字列2)
-		//文字列1と文字列2が同じ時0、異なる時0以外を返す
-		//先頭がvの時、頂点をvertexに追加する
-		if (strcmp(str[0], "v") == 0) {
-			//可変長配列vertexに追加
-			//atof(文字列)　文字列からfloat型の値を返す
+		//先頭がnewmtlの時、マテリアルを追加する
+		if (strcmp(str[0], "newmtl") == 0) {
+			CMaterial* pm = new CMaterial();
+			//マテリアル名の設定
+			pm->Name(str[1]);
+			//マテリアルの可変長配列に追加
+			mpMaterials.push_back(pm);
+			//配列の長さを取得
+			idx = mpMaterials.size() - 1;
 		}
-		else if (strcmp(str[0], "vn") == 0) {
-			//可変長配列normalに追加
-			//atof(文字列)　文字列からfloat型の値を返す
-			normal.push_back(CVector(atof(str[1]), atof(str[2]), atof(str[3])));
+		//先頭がKdの時、Diffuseを設定する
+		else if (strcmp(str[0], "Kd") == 0) {
+			mpMaterials[idx]->Diffuse()[0] = atof(str[1]);
+			mpMaterials[idx]->Diffuse()[1] = atof(str[2]);
+			mpMaterials[idx]->Diffuse()[2] = atof(str[3]);
 		}
-		//先頭がfの時、三角形を作成して追加する
-		else if (strcmp(str[0], "f") == 0) {
-			//頂点と法線の番号作成
-			int v[3], n[3];
-			//頂点と法線の番号取得
-			sscanf(str[1], "%d//%d", &v[0], &n[0]);
-			sscanf(str[2], "%d//%d", &v[1], &n[1]);
-			sscanf(str[3], "%d//%d", &v[2], &n[2]);
-			//三角形作成
-			CTriangle t;
-			t.Vertex(vertex[v[0] - 1], vertex[v[1] - 1], vertex[v[2] - 1]);
-			t.Vertex(normal[n[0] - 1], normal[n[1] - 1], normal[n[2] - 1]);
-			//可変長配列mTrianglesに三角形を追加
-			mTriangles.push_back(t);
+		//先頭がdの時、α値を設定する
+		else if (strcmp(str[0], "d") == 0) {
+			mpMaterials[idx]->Diffuse()[3] = atof(str[1]);
 		}
+
 	}
 
 	//ファイルのクローズ
@@ -136,9 +130,21 @@ void CModel::Load(char* obj, char* mtl) {
 			CTriangle t;
 			t.Vertex(vertex[v[0] - 1], vertex[v[1] - 1], vertex[v[2] - 1]);
 			t.Normal(normal[n[0] - 1], normal[n[1] - 1], normal[n[2] - 1]);
+			//マテリアル番号の設定
+			t.MaterialIdx(idx);
 			//可変長配列mTrianglesに三角形を追加
 			mTriangles.push_back(t);
 		}
+		else if (strcmp(str[0], "usemtl") == 0) {
+			//可変長配列を後から比較
+			for (idx = mpMaterials.size() - 1; idx > 0; idx--) {
+				//同じ名前のマテリアルがあればループ終了
+				if (strcmp(mpMaterials[idx]->Name(), str[1]) == 0) {
+					break; //ループから出る
+				}
+			}
+		}
+
 	}
 
 	//ファイルのクローズ
@@ -149,7 +155,17 @@ void CModel::Load(char* obj, char* mtl) {
 void CModel::Render() {
 	//可変長配列の要素数だけ繰り返し
 	for (int i = 0; i < mTriangles.size(); i++) {
+		//マテリアルの適用
+		mpMaterials[mTriangles[i].MaterialIdx()]->Enabled();
+
 		//可変長配列に添え字でアクセスする
 		mTriangles[i].Render();
+	}
+}
+CModel::~CModel()
+{
+	for(int i =0; i < mpMaterials.size(); i++)
+	{
+		delete mpMaterials[i];
 	}
 }
--- 3DLv1_vs2019_00/GameProgramming/src/CModel.h
+++ 3DLv1_vs2019_00/GameProgramming/src/CModel.h
@@ -4,12 +4,14 @@
 //vectorのインクルード
 #include <vector>
 #include "CTriangle.h"
+#include "CMaterial.h"
 /*
 モデルクラス
 モデルデータの入力や表示
 */
 class CModel {
 public:
+	~CModel();
 	//モデルファイルの入力
 	//Load(モデルファイル名, マテリアルファイル名)
 	void Load(char* obj, char* mtl);
@@ -18,6 +20,9 @@ public:
 private:
 	//三角形の可変長配列
 	std::vector<CTriangle> mTriangles;
+	//マテリアルポインタの可変長配列
+	std::vector<CMaterial*> mpMaterials;
+
 };
 
 #endif
