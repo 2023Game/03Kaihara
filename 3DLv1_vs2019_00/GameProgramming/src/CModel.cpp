b32b6e498657684a1c263609b8283334c164ce8c
kaihara-yuugo-Fukuyama <gc23009@anabuki-net.ne.jp>
2023-10-12 11:29:45 +0900

    課題10修正

--- 3DLv1_vs2019_00/GameProgramming/src/CModel.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CModel.cpp
@@ -84,16 +84,6 @@ void CModel::Load(char* obj, char* mtl) {
 		else if (strcmp(str[0], "map_Kd") == 0) {
 			mpMaterials[idx]->Texture()->Load(str[1]);
 		}
-		//先頭がusemtlの時、マテリアルインデックスを取得する
-		else if (strcmp(str[0], "usemtl") == 0) {
-			//可変長配列を後から比較
-			for (idx = mpMaterials.size() - 1; idx > 0; idx--) {
-				//同じ名前のマテリアルがあればループ終了
-				if (strcmp(mpMaterials[idx]->Name(), str[1]) == 0) {
-					break; //ループから出る
-				}
-			}
-		}
 	}
 
 	//ファイルのクローズ
@@ -183,6 +173,16 @@ void CModel::Load(char* obj, char* mtl) {
 		//atof(文字列)　文字列からfloat型の値を返す
 		uv.push_back(CVector(atof(str[1]), atof(str[2]), 0.0));
 	}
+		//先頭がusemtlの時、マテリアルインデックスを取得する
+	else if (strcmp(str[0], "usemtl") == 0) {
+			//可変長配列を後から比較
+			for (idx = mpMaterials.size() - 1; idx > 0; idx--) {
+				//同じ名前のマテリアルがあればループ終了
+				if (strcmp(mpMaterials[idx]->Name(), str[1]) == 0) {
+					break; //ループから出る
+				}
+			}
+		}
 }
 	//ファイルのクローズ
 	fclose(fp);
