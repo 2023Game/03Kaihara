9f4b601bd9dcb3ab9061dee6985508588eb74d09
kaihara-yuugo-Fukuyama <gc23009@anabuki-net.ne.jp>
2024-07-05 10:28:14 +0900

    課題１８

--- 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
+++ 3DLv1_vs2019_00/GameProgramming/src/CModelX.cpp
@@ -381,6 +381,7 @@ CMesh::CMesh()
 	, mpMaterialIndex(nullptr)
 	, mpAnimateVertex(nullptr)
 	, mpAnimateNormal(nullptr)
+	, mpTextureCoords(nullptr)
 {}
 
 //デストラクタ
@@ -391,6 +392,7 @@ CMesh::~CMesh() {
 	SAFE_DELETE_ARRAY(mpMaterialIndex);
 	SAFE_DELETE_ARRAY(mpAnimateVertex);
 	SAFE_DELETE_ARRAY(mpAnimateNormal);
+	SAFE_DELETE_ARRAY(mpTextureCoords);
 	//スキンウェイトの削除
 	for (size_t i = 0; i < mSkinWeights.size(); i++)
 	{
@@ -518,6 +520,18 @@ void CMesh::Init(CModelX* model) {
 			//CSkinWeightsクラスのインスタンスを作成し、配列に追加
 			mSkinWeights.push_back(new CSkinWeights(model));
 		}
+		//テクスチャ座標の時
+		else if (strcmp(model->Token(), "MeshTextureCoords") == 0) {
+			model->GetToken();	// {
+			//テクスチャ座標数を取得
+			int textureCoordsNum = atoi(model->GetToken()) * 2;
+			//テクスチャ座標のデータを配列に取り込む
+			mpTextureCoords = new float[textureCoordsNum];
+			for (int i = 0; i < textureCoordsNum; i++) {
+				mpTextureCoords[i] = atof(model->GetToken());
+			}
+			model->GetToken();	// }
+		}
 		else {
 			//以外のノードは読み飛ばし
 			model->SkipNode();
@@ -539,10 +553,14 @@ void CMesh::Render()
 	/* 頂点データ，法線データの配列を有効にする */
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_NORMAL_ARRAY);
+	//テクスチャマッピングの配列を有効にする
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
 
 	/* 頂点データ，法線データ，テクスチャ座標の場所を指定する */
 	glVertexPointer(3, GL_FLOAT, 0, mpAnimateVertex);
 	glNormalPointer(GL_FLOAT, 0, mpAnimateNormal);
+	glTexCoordPointer(2, GL_FLOAT, 0, mpTextureCoords);
 	/* 頂点のインデックスの場所を指定して図形を描画する */
 		/* 頂点のインデックスの場所を指定して図形を描画する */
 	for (int i = 0; i < mFaceNum; i++) {
@@ -550,6 +568,7 @@ void CMesh::Render()
 		mMaterial[mpMaterialIndex[i]]->Enabled();
 		glDrawElements(GL_TRIANGLES, 3,
 			GL_UNSIGNED_INT, (mpVertexIndex + i * 3));
+		mMaterial[mpMaterialIndex[i]]->Disabled();
 	}
 
 
@@ -660,11 +679,7 @@ CSkinWeights::CSkinWeights(CModelX* model)
 	}
 	model->GetToken();	// }
 #ifdef _DEBUG
-	printf("SkinWeights %s\n", mpFrameName);
-	for (int i = 0; i < mIndexNum; i++) {
-		printf("%3d %10f\n", mpIndex[i], mpWeight[i]);
-	}
-	mOffset.Print();
+
 #endif
 }
 
@@ -702,7 +717,7 @@ CAnimationSet::CAnimationSet(CModelX* model)
 	mMaxTime = mAnimation[0]->mpKey[mAnimation[0]->mKeyNum - 1].mTime;
 	//デバッグバージョンのみ有効
 #ifdef _DEBUG
-	printf("CAnimationSet %s\n", mpName);
+
 #endif
 }
 
@@ -885,8 +900,7 @@ CAnimation::CAnimation(CModelX* model)
 	}
 
 #ifdef _DEBUG
-		printf("Animation:%s\n", mpFrameName);
-		mpKey[0].mMatrix.Print();
+
 #endif
 }
 
